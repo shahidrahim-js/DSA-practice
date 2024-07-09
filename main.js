e5d8ae1cf4fb84a82ec527f9a970995e30071800
Shahid <shahidpiet7@gmail.com>
2024-07-10 01:02:00 +0530

    Find the average waiting time in a restaurant

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import { isPalindrome } from './problems/palindromeNumber.js';
 import {twoSumWithTarget} from './problems/twoSumWithTarget.js';
+import { averageWaitingTime } from './problems/leetcodeProblems/averageWaitingTime.js';
 
 // Check if given number is Palindrome or not.
 console.log('isPalindrome:', isPalindrome(121));
@@ -13,3 +14,17 @@ const twoSum = twoSumWithTarget(nums, target);
 const twoSum1 = twoSumWithTarget(nums1, target1);
 console.log('twoSum:', twoSum);
 console.log('twoSum1:', twoSum1);
+
+// Average Waiting Time...
+const customers = [
+  [1, 2],
+  [2, 5],
+  [4, 3],
+];
+const customers2 = [
+  [5, 2],
+  [5, 4],
+  [10, 3],
+  [20, 1],
+];
+console.log('averageWaitingTime:', averageWaitingTime(customers));
